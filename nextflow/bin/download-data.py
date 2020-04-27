78a62600f383e99f47c56b2500c5659c3d37eb2c
Chris Cheshire <chris.j.cheshire@gmail.com>
2020-04-27 13:58:57 +0100

    download data now works with nextflow

--- nextflow/bin/download-data.py
+++ nextflow/bin/download-data.py
@@ -1,19 +1,12 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
 # Import libs
 import os.path
 from os import path
 import gzip
 import urllib.request
 from biomart import BiomartServer
-
-
-# In[2]:
-
 
 def download_file(url, file_path_gz, file_path):
     if path.exists(file_path) is False: 
@@ -25,26 +18,18 @@ def download_file(url, file_path_gz, file_path):
         output.write(s)
         output.close()
 
-
-# In[3]:
-
-
 # Setup
-data_path = '../data'
-gtf_path_gz = data_path + '/Mus_musculus.GRCm38.99.gtf.gz'
-gtf_path = data_path + '/Mus_musculus.GRCm38.99.gtf'
-tpm_yang_path_gz = data_path + '/GSE90848_Ana6_basal_hair_bulb_TPM.txt.gz'
-tpm_yang_path = data_path + '/GSE90848_Ana6_basal_hair_bulb_TPM.txt'
-tpm_yang_path2_gz = data_path + '/GSE90848_Tel_Ana1_Ana2_bulge_HG_basal_HB_TPM.txt.gz'
-tpm_yang_path2 = data_path + '/GSE90848_Tel_Ana1_Ana2_bulge_HG_basal_HB_TPM.txt'
-tpm_joost_path_gz = data_path + '/GSE67602_Joost_et_al_expression.txt.gz'
-tpm_joost_path = data_path + '/GSE67602_Joost_et_al_expression.txt'
-tpm_ghahramani_path_gz = data_path + '/GSE99989_NCA_BCatenin_TPM_matrix.csv.gz'
-tpm_ghahramani_path = data_path + '/GSE99989_NCA_BCatenin_TPM_matrix.csv'
-gene_tsv_path = data_path + '/gene_names.tsv'
-
-
-# In[4]:
+gtf_path_gz = 'Mus_musculus.GRCm38.99.gtf.gz'
+gtf_path =  'Mus_musculus.GRCm38.99.gtf'
+tpm_yang_path_gz = 'GSE90848_Ana6_basal_hair_bulb_TPM.txt.gz'
+tpm_yang_path = 'GSE90848_Ana6_basal_hair_bulb_TPM.txt'
+tpm_yang_path2_gz = 'GSE90848_Tel_Ana1_Ana2_bulge_HG_basal_HB_TPM.txt.gz'
+tpm_yang_path2 = 'GSE90848_Tel_Ana1_Ana2_bulge_HG_basal_HB_TPM.txt'
+tpm_joost_path_gz = 'GSE67602_Joost_et_al_expression.txt.gz'
+tpm_joost_path = 'GSE67602_Joost_et_al_expression.txt'
+tpm_ghahramani_path_gz = 'GSE99989_NCA_BCatenin_TPM_matrix.csv.gz'
+tpm_ghahramani_path = 'GSE99989_NCA_BCatenin_TPM_matrix.csv'
+gene_tsv_path = 'gene_names.tsv'
 
 
 # Download initial datasets and decompress
@@ -53,10 +38,6 @@ download_file('ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE90nnn/GSE90848/suppl/GSE
 download_file('ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE90nnn/GSE90848/suppl/GSE90848_Tel_Ana1_Ana2_bulge_HG_basal_HB_TPM.txt.gz', tpm_yang_path2_gz, tpm_yang_path2)
 download_file('ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE67nnn/GSE67602/suppl/GSE67602_Joost_et_al_expression.txt.gz', tpm_joost_path_gz, tpm_joost_path)
 download_file('ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE99nnn/GSE99989/suppl/GSE99989_NCA_BCatenin_TPM_matrix.csv.gz', tpm_ghahramani_path_gz, tpm_ghahramani_path)
-
-
-# In[12]:
-
 
 # Get refseq gene ids
 if path.exists(gene_tsv_path) is False: 
@@ -68,4 +49,3 @@ if path.exists(gene_tsv_path) is False:
 
     with open(gene_tsv_path, 'wb') as output:
         output.write(search_response.raw.data)
-
--- nextflow/main.nf
+++ nextflow/main.nf
@@ -26,17 +26,45 @@ nextflow.preview.dsl=2
 //params.results = "$baseDir/test/data/results"
 
 /*------------------------------------------------------------------------------------*/
+/* Processes
+--------------------------------------------------------------------------------------*/
+
+/*gtf_path_gz = 'Mus_musculus.GRCm38.99.gtf.gz'
+tpm_yang_path_gz = 'GSE90848_Ana6_basal_hair_bulb_TPM.txt.gz''
+tpm_yang_path2_gz = 'GSE90848_Tel_Ana1_Ana2_bulge_HG_basal_HB_TPM.txt.gz'
+tpm_joost_path_gz = 'GSE67602_Joost_et_al_expression.txt.gz'
+tpm_ghahramani_path_gz = 'GSE99989_NCA_BCatenin_TPM_matrix.csv.gz'
+tpm_ghahramani_path = 'GSE99989_NCA_BCatenin_TPM_matrix.csv'
+gene_tsv_path = 'gene_names.tsv'
+
+gtf_path = data_path + '/Mus_musculus.GRCm38.99.gtf'
+tpm_yang_path = data_path + '/GSE90848_Ana6_basal_hair_bulb_TPM.txt'
+tpm_yang_path2 = data_path + '/GSE90848_Tel_Ana1_Ana2_bulge_HG_basal_HB_TPM.txt'
+tpm_joost_path = data_path + '/GSE67602_Joost_et_al_expression.txt'
+tpm_ghahramani_path = data_path + '/GSE99989_NCA_BCatenin_TPM_matrix.csv'
+gene_tsv_path = data_path + '/gene_names.tsv'
+gtf_data_path = data_path + '/gtf.txt'*/
+
+process download {
+  publishDir "${params.outdir}/data",
+    mode: "copy", overwrite: true
+
+    output:
+      path "*.{txt,tsv,csv,gtf}"
+
+    script:
+    """
+    python $baseDir/bin/download-data.py
+    """
+}
+
+/*------------------------------------------------------------------------------------*/
 
 // Run workflow
 workflow {
 
-    // Create channels for indices
-    ch_bowtieIndex = Channel.fromPath( params.bowtie_index )
-    ch_starIndex = Channel.fromPath( params.star_index )
-    ch_genomeFai = Channel.fromPath( params.genome_fai )
-
-    // Get fastq paths 
-    metadata( params.input )
+    // Download data
+    download().collect().view()
 }
 
 
