7c41f8c6866b7eb367bc18c900504dd00aa442a0
Chris Cheshire <chris.j.cheshire@gmail.com>
2020-04-28 11:01:02 +0100

    updated config

---
+++ run_scripts/run-crick.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+## LOAD REQUIRED MODULES
+ml purge
+ml Nextflow/20.01.0
+ml Singularity/3.4.2
+ml Graphviz
+
+## UPDATE PIPLINE
+nextflow pull luslab/arshamg-scrnaseq-wgan
+
+## RUN PIPELINE
+nextflow run luslab/arshamg-scrnaseq-wgan \
+  -profile crick \
+  --epochs 2 \
+  --writeFreq 1
