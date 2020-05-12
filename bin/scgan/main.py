ed6cbddc83a694f7d9fa113d9bfd200da6773f2a
Chris Cheshire <chris.j.cheshire@gmail.com>
2020-05-12 11:00:38 +0100

    Added template

--- bin/scgan/main.py
+++ bin/scgan/main.py
@@ -4,3 +4,21 @@
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
+
+import argparse
+
+if __name__ == '__main__':
+    """
+    Main comment
+    """
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--dataPath', required=False,
+                        help='Path to working folder')
+
+    parser.add_argument(
+        '--pbmc-convert', required=False,
+        default=False, action='store_true',
+        help='Pre convert PBMC base data')
+
+    parsedArgs = parser.parse_args()
