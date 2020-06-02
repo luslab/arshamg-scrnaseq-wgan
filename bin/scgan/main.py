72104952ade6225c2ed1fbf2d0c0db16f2504ccf
Chris Cheshire <chris.j.cheshire@gmail.com>
2020-06-02 12:03:54 +0100

    Fixes for dataset loading

--- bin/scgan/main.py
+++ bin/scgan/main.py
@@ -22,7 +22,6 @@ from natsort import natsorted
 import tensorflow as tf
 
 sct = collections.namedtuple('sc', ('barcode', 'count_no', 'genes_no', 'dset', 'cluster'))
-
 logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
 
 SEED = 0
@@ -38,7 +37,18 @@ params_pre_test_cells = 0
 params_pre_valid_cells = 2000
 params_train_split_files = 10
 
+params_train_gene_count = 17789
+params_train_dataset_buffer_size = 1000
+params_train_dataset_batch_size = 32
+
+
 def pre_pbmc_convert():
+    # MTX files
+    # https://math.nist.gov/MatrixMarket/formats.html
+    # Format for sparse matrices that encode for the 
+    # table coordinates of non zero data only (3 column file - x,y,value)
+    # The tsv files encode the rows and columns (barcoded cells and matched genes)
+
     # Log
     logger = logging.getLogger("pbmc-conv")
     logger.info('Converting pbmc files...')
@@ -60,7 +70,7 @@ def pre_pbmc_convert():
     logger.info(obs_names_path)
     logger.info(output_h5ad_path)
 
-    # Load gene names
+    # Load gene names into array
     with open(var_names_path, "r") as var_file:
         var_read = csv.reader(var_file, delimiter='\t')
         var_names = []
@@ -68,19 +78,19 @@ def pre_pbmc_convert():
             var_names.append(row[1])
     logger.info("Loaded " + str(len(var_names)) + " gene names")
 
-    # Load UMIs
+    # Load cell barcodes into array
     with open(obs_names_path, "r") as obs_file:
         obs_read = csv.reader(obs_file, delimiter='\t')
         obs_names = []
         for row in obs_read:
             obs_names.append(row[0])
-    logger.info("Loaded " + str(len(obs_names)) + " UMIs")
+    logger.info("Loaded " + str(len(obs_names)) + " barcodes")
 
-    # Load 10x data and convert
+    # Load count matrix data
     andata = sc.read(data_path) 
     andata = andata.transpose()
 
-    # Make var names unique
+    # Make var names unique (appends numbers to duplicates)
     andata.var_names = var_names
     andata.var_names_make_unique()
     andata.obs_names = obs_names
@@ -99,12 +109,12 @@ def pre_pbmc_process():
     output_h5ad_file = "68kPBMCs_processed.h5ad"
     h5ad_path = os.path.join(params_datapath, h5ad_file)
     output_h5ad_path = os.path.join(params_datapath, output_h5ad_file)
-
    
     # Load single cell data
     sc_raw = sc.read(h5ad_path)
 
     # appends -1 -2... to the name of genes that already exist
+    # this is already done in previous function
     sc_raw.var_names_make_unique()
     if sp_sparse.issparse(sc_raw.X):
         sc_raw.X = sc_raw.X.toarray()
@@ -153,22 +163,28 @@ def pre_pbmc_process():
     random.seed(SEED)
     np.random.seed(SEED)
 
+    # Calc how many validation cells there should be per cluster
     valid_cells_per_cluster = {
         key: int(value * params_pre_valid_cells)
         for key, value in clusters_ratios.items()}
 
+    # Calc how many test cells there should be per cluster
     test_cells_per_cluster = {
         key: int(value * params_pre_test_cells)
         for key, value in clusters_ratios.items()}
 
+    # Create a obs column for the split of the train/test valid data
+    # Initialise to train and set it as a categorical column
     dataset = np.repeat('train', sc_raw.shape[0])
     unique_groups = np.asarray(['valid', 'test', 'train'])
     sc_raw.obs['split'] = pd.Categorical(values=dataset, categories=natsorted(unique_groups))
 
+    # For each cluster 
     for key in valid_cells_per_cluster:
-        # all cells from clus idx
+        # Get all the cells that match the cluster id
         indices = sc_raw.obs[sc_raw.obs['cluster'] == str(key)].index
 
+        # Randomly assign a number of cells to the test and valid sets
         test_valid_indices = np.random.choice(
             indices, valid_cells_per_cluster[key] +
             test_cells_per_cluster[key], replace=False)
@@ -176,6 +192,7 @@ def pre_pbmc_process():
         test_indices = test_valid_indices[0:test_cells_per_cluster[key]]
         valid_indices = test_valid_indices[test_cells_per_cluster[key]:]
 
+        # assign the test/training split
         for i in test_indices:
             #sc_raw.obs.set_value(i, 'split', 'test')
             sc_raw.obs.at[i, 'split'] = 'test'
@@ -188,6 +205,10 @@ def pre_pbmc_process():
     sc_raw.write(output_h5ad_path)
 
 def pre_pbmc_write_tf():
+    # Log
+    logger = logging.getLogger("pbmc-tf")
+    logger.info('Preprocessing pbmc files to TF records...')
+
     h5ad_file = "68kPBMCs_processed.h5ad"
     h5ad_path = os.path.join(params_datapath, h5ad_file)
     train_filenames = [os.path.join(params_datapath, "tf", 'train-%s.tfrecords' % i)
@@ -197,24 +218,23 @@ def pre_pbmc_write_tf():
 
     # Load single cell data
     sc_raw = sc.read(h5ad_path)
-    print(sc_raw.shape[0])
+    cell_count = sc_raw.shape[0]
+    gene_count = sc_raw.shape[1]
+    logger.info("Cells number is %d , with %d genes per cell." % (cell_count, gene_count))
 
     # Create TF Writers
-    opt = tf.python_io.TFRecordOptions(tf.python_io.TFRecordCompressionType.GZIP)
-    valid = tf.python_io.TFRecordWriter(valid_filename, opt)
-    test = tf.python_io.TFRecordWriter(test_filename, opt)
-    train = [tf.python_io.TFRecordWriter(filename, opt) for filename in train_filenames]
+    opt = tf.io.TFRecordOptions(compression_type='GZIP')
+    valid = tf.io.TFRecordWriter(valid_filename, opt)
+    test = tf.io.TFRecordWriter(test_filename, opt)
+    train = [tf.io.TFRecordWriter(filename, opt) for filename in train_filenames]
 
     cat = sc_raw.obs['cluster'].cat.categories
-    err_count = 0
     count = 0
     for line in sc_raw:
-        count += 1
-        if len(line.obs['split']) == 0:
-            err_count += 1
-            print("Error - " + str(count))
-            #print(line.obs_names[0])
+        # Doesnt stop for some reason
+        if count == cell_count:
             break
+        count += 1
 
         dset = line.obs['split'][0]
 
@@ -239,24 +259,20 @@ def pre_pbmc_write_tf():
         feat_map['genes_no'] = tf.train.Feature(int64_list=tf.train.Int64List(value=[d.genes_no]))
         feat_map['count_no'] = tf.train.Feature(int64_list=tf.train.Int64List(value=[d.count_no]))
 
-        example = tf.train.Example(features=tf.train.Features(feature=feat_map))
-
         # add hot encoding for classification problems
         #feat_map['cluster_1hot'] = tf.train.Feature(
         #    int64_list=tf.train.Int64List(value=[int(c == cat) for c in cat]))
         #feat_map['cluster_int'] = tf.train.Feature(int64_list=tf.train.Int64List(value=[int(cat)]))
 
-        strings = [example.SerializeToString()]
+        example = tf.train.Example(features=tf.train.Features(feature=feat_map))
+        example_str = example.SerializeToString()
 
         if d.dset == 'test':
-            for s in strings:
-                test.write(s)
+                test.write(example_str)
         elif d.dset == 'train':
-            for s in strings:
-                train[random.randint(0, params_train_split_files - 1)].write(s)
+                train[random.randint(0, params_train_split_files - 1)].write(example_str)
         elif d.dset == 'valid':
-            for s in strings:
-                valid.write(s)
+                valid.write(example_str)
         else:
             raise ValueError("invalid dataset: %s" % d.dset)
 
@@ -266,7 +282,40 @@ def pre_pbmc_write_tf():
     for f in train:
         f.close()
 
-    print(err_count)
+feature_map = {'scg': tf.io.SparseFeature(
+        index_key='indices',
+        value_key='values',
+        dtype=tf.float32,
+        size=params_train_gene_count)}
+
+def _parse_example(example_str):
+  example = tf.io.parse_single_example(example_str, feature_map)
+  return tf.sparse.to_dense(example['scg'])
+
+
+def train_pbmc():
+    print("Num CPUs Available: ", len(tf.config.experimental.list_physical_devices('CPU')))
+    print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
+
+    # find training and validation TF records
+    tf_dir = os.path.join(params_datapath, 'tf')
+    train_files = [os.path.join(tf_dir, f)
+                   for f in os.listdir(tf_dir) if "train" in f]
+    valid_files = [os.path.join(tf_dir, f)
+                   for f in os.listdir(tf_dir) if "valid" in f]
+
+    print("Found " + str(len(train_files)) + " train files")
+    print("Found " + str(len(valid_files)) + " valid files")
+
+    raw_dataset = tf.data.TFRecordDataset(train_files, compression_type='GZIP')
+
+    dataset = raw_dataset.map(_parse_example) \
+                         .shuffle(params_train_dataset_buffer_size) \
+                         .batch(params_train_dataset_batch_size)
+                         #.batch(params_train_dataset_batch_size, drop_remainder=True)
+
+
+
 
 if __name__ == '__main__':
     """
@@ -292,6 +341,11 @@ if __name__ == '__main__':
         default=False, action='store_true',
         help='Write PBMC data to TF records')
 
+    parser.add_argument(
+        '--pbmc_train', required=False,
+        default=False, action='store_true',
+        help='Train on PBMC data')
+
     parsedArgs = parser.parse_args()
 
     params_datapath = parsedArgs.data_path
@@ -306,4 +360,8 @@ if __name__ == '__main__':
 
     if parsedArgs.pbmc_tf:
         pre_pbmc_write_tf()
+        sys.exit
+
+    if parsedArgs.pbmc_train:
+        train_pbmc()
         sys.exit
